2eeb896834b08a3dda3772978c47b085aad5d12b
Mara Sybesma <mara.sybesma@gmail.com>
2025-02-15 22:49:31 -0600

    oh boy its checking time

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os
 
 app = Flask(__name__)
 
-print("all environment variables")
-print(os.environ)
-
 connection = psycopg2.connect(
     host=os.environ.get("PGHOST"),
     user=os.environ.get("POSTGRES_USER"),
@@ -15,14 +12,28 @@ connection = psycopg2.connect(
     database=os.environ.get("PGDATABASE")
 )
 
+def sortByPrice(e):
+    return e[4]
+
 @app.route('/')
-def index():
+def getAll():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM items;")
-            items = cursor.fetchall()
+            items = cursor.fetchall().sort(key=sortByPrice)
     return jsonify(items)
 
+@app.route('/check/<int:id>')
+def check(id: int):
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE items SET checked = TRUE WHERE id = {id}" )
+                
+        return jsonify({"success":True}), 200
+
+    except:
+        return jsonify({"error": "oopsie woopsie! uwu we made a fucky wucky!! a wittle fucko boingo!"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
